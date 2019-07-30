6377f9ac8fd81081760c89308f1240f663f5c734
mac <mac@marchewka.local>
2019-07-30 16:34:34 +0200

    Added repo to data

--- src/data/repos.js
+++ src/data/repos.js
@@ -2,7 +2,13 @@ export default [
   {
     id: 1,
     title: "book-store",
-    description: "Book store built with React and Redux ",
+    description: "Book store built with React and Redux",
     language: "TypeScript",
+  },
+  {
+    id: 1,
+    title: "my-page",
+    description: "Personal static website running on Gatsby and React",
+    language: "JavaScript",
   },
 ]
